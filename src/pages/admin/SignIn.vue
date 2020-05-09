91a29278e020e1fcbdf1d3f76ceda1fe9394e0b7
nnewnew <godsnew2542@gmail.com>
2020-05-09 20:12:51 +0700

    test branch

--- src/pages/admin/SignIn.vue
+++ src/pages/admin/SignIn.vue
@@ -1,8 +1,23 @@
 <template>
   <q-page>
-      <h4>SignIn</h4>
+    <div class="q-pa-md row">
+      <div class="col"></div>
+      <q-card class="my-card col-12 col-md-4">
+        <q-card-section class="text-center">Log in to you Admin</q-card-section>
+        <div class="col-3">
+          <q-input filled v-model="email" type="email" suffix="@gmail.com" >
+            <template v-slot:before>
+              <q-icon name="mail" />
+            </template>
+          </q-input>
+        </div>
+        <br />
+      </q-card>
+      <div class="col"></div>
+    </div>
   </q-page>
 </template>
+
 
 <script>
 export default {
