2900bc9aa008eb5595cb55155e6e9b92248d9ebd
nnewnew <godsnew2542@gmail.com>
2020-05-11 11:44:58 +0700

    ทำหน้า dashboaed and shoplist

--- src/pages/admin/Dashboard.vue
+++ src/pages/admin/Dashboard.vue
@@ -2,19 +2,57 @@
   <q-page class="q-pa-md">
     <h4 class="text-center">ภาพรวมร้านค้า</h4>
 
-    <q-card class="bg-secondary text-white">
-      <q-card-section>
-        <div class="text-h6">Our Changing Planet</div>
-        <div class="text-subtitle2">by John Doe</div>
-      </q-card-section>
+    <div class="row items-start q-gutter-md text-center">
+      <div class="col-1"></div>
 
-      <q-card-section>lorem</q-card-section>
-    </q-card>
+      <div class="col-11 col-md-3">
+        <q-card class="bg-orange-6 text-white">
+          <q-card-section>
+            <div class="text-h6">ร้านค้า 50 ร้าน</div>
+            <!-- <div class="text-subtitle2">by John Doe</div> -->
+          </q-card-section>
+
+          <!-- <q-card-section>lorem</q-card-section> -->
+        </q-card>
+      </div>
+
+      <div class="col-11 col-md-3">
+        <q-card class="bg-blue text-white">
+          <q-card-section>
+            <div class="text-h6">ผู้ใช้งาน 600 คน</div>
+          </q-card-section>
+        </q-card>
+      </div>
+
+      <div class="col-11 col-md-3">
+        <q-card class="bg-purple-6 text-white">
+          <q-card-section>
+            <div class="text-h6">สินค้า 1562 รายการ</div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+    <!-- โยงหน้า -->
+    <br>
+    <div class="text-center">
+      <q-btn @click="openList" label="Go Shop List" type="submit" color="blue-10"></q-btn>
+    </div>
+
   </q-page>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    openList() {
+      console.log("openList");
+      this.$router.push({ name: "shoplist" });
+    }
+  }
+};
 </script>
 
 <style>
--- src/pages/admin/ShopList.vue
+++ src/pages/admin/ShopList.vue
@@ -1,15 +1,110 @@
 <template>
-  <q-page>
-      <h4>ShopList</h4>
+  <q-page class="q-pa-md">
+    <h4 class="text-center">
+      รายชื่อร้านค้า
+      <hr />
+    </h4>
+
+    <q-card>
+      <q-tabs
+        v-model="tab"
+        dense
+        class="text-grey"
+        active-color="dark"
+        indicator-color="primary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab name="mails" label="รออนุมัติ"></q-tab>
+        <q-tab name="alarms" label="ทั้งหมด"></q-tab>
+      </q-tabs>
+
+      <q-separator></q-separator>
+
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="mails">
+          <q-btn color="primary" @click="showTextLoading">จัดการ</q-btn>
+
+          <q-card class="bg-grey-3 relative-position card-example" v-show="showSimulatedReturnData">
+            <q-card-section class="q-pb-none">
+              <div class="text-h6">Lorem Ipsum</div>
+            </q-card-section>
+
+            <q-card-section>
+              <transition
+                appear
+                enter-active-class="animated fadeIn"
+                leave-active-class="animated fadeOut"
+              >
+                <div>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel magna eu risus laoreet tristique.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel magna eu risus laoreet tristique.
+                </div>
+              </transition>
+            </q-card-section>
+
+            <q-inner-loading :showing="visible">
+              <q-spinner-gears size="500px" color="primary"></q-spinner-gears>
+            </q-inner-loading>
+          </q-card>
+        </q-tab-panel>
+
+        <q-tab-panel name="alarms">
+          <div class="q-pa-md q-gutter-lg">
+            <q-btn label="จัดการ" color="primary" @click="confirm"></q-btn>
+          </div>
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
   </q-page>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      tab: "mails",
 
-}
+      visible: false,
+      showSimulatedReturnData: false
+    };
+  },
+  methods: {
+    showTextLoading() {
+      this.visible = true;
+      this.showSimulatedReturnData = false;
+      setTimeout(() => {
+        this.visible = false;
+        this.showSimulatedReturnData = true;
+      }, 1000);
+    },
+    confirm() {
+      this.$q
+        .dialog({
+          title: "ชื่อร้าน",
+          message: "ข้อมูล",
+          cancel: true,
+          persistent: true
+        })
+        .onOk(() => {
+          // console.log('>>>> OK')
+        })
+        .onOk(() => {
+          // console.log('>>>> second OK catcher')
+        })
+        .onCancel(() => {
+          // console.log('>>>> Cancel')
+        })
+        .onDismiss(() => {
+          // console.log('I am triggered on both OK and Cancel')
+        });
+    }
+  }
+};
 </script>
 
 <style>
-
+.card-example {
+  width: 500px;
+}
 </style>
