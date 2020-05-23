a383d4c5aefd01e5c75faf20274afb9d15e8b395
nnewnew <godsnew2542@gmail.com>
2020-05-23 17:19:50 +0700

    Add text and layout

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,38 +1,31 @@
 <template>
-  <q-page class="q-pa-md">
-    <h6>หลาดชุมทางทุ่งส่งสนับสนุนโดย</h6>
-    <div class="row q-gutter-md" style="padding: 15px 15px 15px 0px;">
-      <!-- <div class="row"> -->
-      <!-- <div class="col-12 col-md-2"> -->
-      <q-card class="col-12 col-md-2">
-        <q-img src="../assets/แผนงานการพัฒนา.jpg" />
-      </q-card>
-      <!-- </div> -->
+  <q-page>
+    <div class="text-h6 text-center q-ma-md">หลาดชุมทางทุ่งสงสนับสนุนโดย</div>
 
-      <!-- <div class="col-12 col-md-2"> -->
-      <q-card class="col-12 col-md-2">
-        <q-img q-img src="../assets/มหาวิทยาลัย.jpg" />
-      </q-card>
-      <!-- </div> -->
-
-      <!-- <div class="col-12 col-md-2"> -->
-      <q-card class="col-12 col-md-2">
-        <q-img src="../assets/ประชาคม.jpg" />
-      </q-card>
-      <!-- </div> -->
-
-      <!-- <div class="col-12 col-md-2"> -->
-      <q-card class="col-12 col-md-2">
-        <q-img src="../assets/เทศบาลเมือง.jpg" />
-      </q-card>
-      <!-- </div> -->
-
-      <!-- <div class="col-12 col-md-2"> -->
-      <q-card class="col-12 col-md-2">
-        <q-img src="../assets/สกสว.jpg" style="height:200px;" />
-      </q-card>
-      <!-- </div> -->
-    </div>
+    <!-- <div class="row q-gutter-md justify-center" style="padding: 15px 15px 15px 0px;"> -->
+    <q-card class="row justify-center" style="padding: 15px 15px 15px 0px;">
+      <div class="col-12 col-md-2">
+        <q-img src="../assets/spon1.jpg" class="img" contain />
+      </div>
+      <div class="col-12 col-md-2">
+        <q-img src="../assets/spon2.jpg" class="img" contain />
+      </div>
+      <div class="col-12 col-md-2">
+        <q-img src="../assets/spon3.jpg" class="img" contain />
+      </div>
+      <div class="col-12 col-md-2">
+        <q-img src="../assets/spon4.jpg" class="img" contain />
+      </div>
+      <div class="col-12 col-md-2">
+        <q-img src="../assets/spon5.jpg" class="img" contain />
+      </div>
+    </q-card>
+    <!-- <div class="col-12 col-md-2 m1" style="height:300px;"></div>
+    <div class="col-12 col-md-2 m2" style="height:300px"></div>
+    <div class="col-12 col-md-2 m3" style="height:300px"></div>
+    <div class="col-12 col-md-2 m4" style="height:300px"></div>
+    <div class="col-12 col-md-2 m5" style="height:300px"></div>-->
+    <!-- </div> -->
   </q-page>
 </template>
 
@@ -41,4 +34,7 @@ export default {};
 </script>
 
 <style>
+.img {
+  height: 280px;
+}
 </style>
