a265344fb37d3f7274943ab2190ccdc605b28e0e
Korawit <korawit.p@gmail.com>
2020-06-19 00:33:13 +0700

    Added sponsor logo

--- src/layouts/Splash.vue
+++ src/layouts/Splash.vue
@@ -1,13 +1,33 @@
 <template>
   <div>
     <div v-if="loading" class="window-height window-width row justify-center items-center bg-white">
-      <div class="column" style="height: 350px">
-        <div class="col-8">
+      <div class="row" style="margin-top: 50px">
+        <div class="col-12 text-center">
           <q-img src="~assets/logo.png" style="width: 250px"></q-img>
         </div>
-        <div class="col-2 justify-center text-center">
+        <div class="col-12 text-center">
           <q-spinner-dots color="secondary" size="5em" />
           <q-tooltip :offset="[0, 8]">Loading</q-tooltip>
+        </div>        
+      </div>
+      <div class="row">
+        <div class="col-12 text-center">
+          <div class="text-h6">สนับสนุนโดย</div>
+        </div>
+        <div class="col-4 text-center q-pa-sm">
+          <q-img src="~assets/spon1.jpg" style="width: 80px"></q-img>
+        </div>
+        <div class="col-4 text-center q-pa-sm">
+          <q-img src="~assets/spon4.jpg" style="width: 70px"></q-img>
+        </div>
+        <div class="col-4 text-center q-pa-sm">
+          <q-img src="~assets/spon2.jpg" style="width: 80px"></q-img>
+        </div>
+        <div class="col-6 text-center q-pa-sm">
+          <q-img src="~assets/spon5.jpg" style="width: 80px"></q-img>
+        </div>
+        <div class="col-6 text-center q-pa-sm">
+          <q-img src="~assets/spon3.jpg" style="width: 150px"></q-img>
         </div>
       </div>
     </div>
@@ -25,7 +45,6 @@ export default {
     };
   },
   async mounted() {
- 
     if (this.$firebase.auth().currentUser !== null) {
       console.log(this.$firebase.auth().currentUser);
       if (!this.$firebase.auth().currentUser.phoneNumber) {
